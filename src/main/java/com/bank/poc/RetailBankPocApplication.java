013da068cef9541365985fed77a63f7020fbadc6
xiexyo <2272489099@qq.com>
2026-04-08 13:21:43 +0800

    first commit

---
+++ src/main/java/com/bank/poc/RetailBankPocApplication.java
@@ -0,0 +1,13 @@
+package com.bank.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailBankPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailBankPocApplication.class, args);
+
+    }
+}
